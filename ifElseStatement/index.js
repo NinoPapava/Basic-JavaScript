32fe93e526831c1f589e168404b9b541c1336182
NinoPapava <gv.papava@gmail.com>
2024-01-09 05:07:55 +0400

    update if/else statement

--- ifElseStatement/index.js
+++ ifElseStatement/index.js
@@ -1,13 +1,18 @@
-const userIsAdmin = true;
+const userIsAdmin = false;
+const userIsStudent = true;
+
 let discount;
 
 if (userIsAdmin) {
   console.log("You are admin!");
   discount = 25;
+} else if(userIsStudent){
+  console.log("You are student!");
+  discount = 15;
 } else {
   console.log("You are user!");
   discount = 0;
 }
 
-console.log("Your discount: " + discount);
+console.log("Your discount: " + discount + "%");
 console.log("THE END!");
