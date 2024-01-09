8ea3209c92aef70b27f25296c877ca4d4b63d290
NinoPapava <gv.papava@gmail.com>
2024-01-09 04:19:44 +0400

    update ternary operator in JS

--- ternaryOperator/index.js
+++ ternaryOperator/index.js
@@ -1,5 +1,8 @@
 const userIsAdmin = false;
-
 const discount = userIsAdmin ? 25 : 10;
+console.log("Discount: " + discount);
 
-console.log("Discount is " + discount);
+const userDistance = 45;
+const shippingCost = userDistance <= 50 ? 10 : 15;
+console.log("Shipping cost: " + shippingCost + "₾");
+
