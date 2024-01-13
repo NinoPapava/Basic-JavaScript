062906dc8f37097ec9798f4c5329e93901f4a770
NinoPapava <gv.papava@gmail.com>
2024-01-13 18:38:13 +0400

    DOM and Windows API

---
+++ Dom&windowsAPI/js/script.js
@@ -0,0 +1,7 @@
+// first paragraph
+const aboutUs = document.querySelector('.container');
+// all paragraph in array
+const texts = document.querySelectorAll('.container');
+
+console.log(aboutUs);
+console.log(texts);
