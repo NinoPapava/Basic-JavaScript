88c771211adac1abbbcf7a4af99395c23ec4e460
NinoPapava <gv.papava@gmail.com>
2024-01-15 21:04:26 +0400

    What Can JavaScript Do

---
+++ usingJavaScript/js/script.js
@@ -0,0 +1,19 @@
+function bigText (){
+  document.getElementById('demo').style.fontSize='35px';
+}
+
+function smallText (){
+  document.getElementById('demo').style.fontSize='15px';
+}
+
+function hideDemo (){
+  document.getElementById('demo').style.display='none';
+}
+
+function showDemo (){
+  document.getElementById('demo').style.display='block';
+}
+
+function date (){
+  document.getElementById('demo2').innerHTML = Date();
+}
