bf4fa416c642f5de81cbf2b0da35936164e07c5a
NinoPapava <gv.papava@gmail.com>
2024-01-16 14:49:42 +0400

    JavaScript Output

---
+++ javascriptOutput/js/script.js
@@ -0,0 +1,20 @@
+//  Using innerHTML 
+document.getElementById("demo").innerHTML = 5 + 6;
+
+
+// delete all existing HTML
+function writeFunction() {
+  document.write(5 + 6);
+}
+
+// Using window.alert()
+window.alert(5 + 6);
+
+// We can skip the window keyword
+// alert(5 + 6);
+
+// Using console.log()
+console.log(5 + 6);
+
+// JavaScript Print
+// <button onclick="window.print()">Print this page</button>
