49998e0f013c18e0dab4d599566d0f70e16962e3
NinoPapava <gv.papava@gmail.com>
2024-01-27 15:11:43 +0400

    BitCamp exercises - chapter I

---
+++ BitCampExercises/chapter-I/index.js
@@ -0,0 +1,44 @@
+// A program that will output "Hello World" to the console
+console.log("Hello World");
+
+// Let's create a variable and assign it a number
+let number = 15;
+// Let's print the value
+console.log(number);
+
+// A program to add two numbers and print the result
+let x = 30;
+let y = 25;
+let result = x + y;
+console.log(result);
+
+// A program that subtracts two numbers from each other and displays the result
+result = x - y;
+console.log(result);
+
+// A program to multiply two numbers and display the result
+result = x * y;
+console.log(result);
+
+// A program to divide two numbers and print the result
+result = x / y;
+console.log(result);
+
+// A program to calculate the remainder of the division of two numbers
+result = x % y;
+console.log(result);
+
+// variable and assign it a string. Print the length of the string
+let string = "Hello, world. ";
+console.log(string.length);
+
+// A program to concatenate two strings and display the result
+let string2 = "I am Gvantsa."
+result = string + string2;
+console.log(result);
+
+// A program to convert a string to uppercase letters
+console.log(result.toUpperCase());
+
+// A program to convert a string to lowercase
+console.log(result.toLowerCase());
--- Dom&windowsAPI/js/script.js
+++ Dom&windowsAPI/js/script.js
@@ -1,7 +1,1 @@
-// first paragraph
-const aboutUs = document.querySelector('.container');
-// all paragraph in array
-const texts = document.querySelectorAll('.container');
-
-console.log(aboutUs);
-console.log(texts);
+const aboutUs = document.getElementById('aboutus');
