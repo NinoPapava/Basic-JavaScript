ef9914c28e1fe68ab444472826f2bbd9e48535d9
NinoPapava <gv.papava@gmail.com>
2024-02-08 16:39:16 +0400

    add events

--- events/js/script.js
+++ events/js/script.js
@@ -3,6 +3,12 @@ function doSomething() {
   console.log("container clicked!");
 }
 
+// function containerClick() {
+//   console.log("clicked!");
+// }
+// const containerClick = document.querySelector("#clicked");
+// containerClick.addEventListener("click", containerClick);
+
 // onScroll
 function scroll() {
   console.log("container scrolled!");
